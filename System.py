e4d6bb4dfbdf8df8e87cb4efcae64485f82dced0
reginaleto <ginaleto@umich.edu>
2023-10-08 14:12:43 -0400

    Testing for Instruction_Decode

--- System.py
+++ System.py
@@ -1,21 +1,22 @@
 import Instruction_Decode as Barcode
-import Payload_Manipulation as Forklift
-import Motor_Control as Motors
-import Streaming
-import Sensor_Integration as Input
+# import Payload_Manipulation as Forklift
+# import Motor_Control as Motors
+# import Streaming
+# import Sensor_Integration as Input
 
 
 
-class System(Streaming.Streaming_GUI):
+# class System(Streaming.Streaming_GUI):
+class System():
     def __init__(self):
-        self.Payload_Color = 'red'
-        self.ID = '1'
-        
-    #def Start_System():
+        self.Payload_Color = 'green'
+        self.ID = ''
+            
+        #def Start_System():
     
-    def Main(): 
-        # Barcode.Instruction_Algorithm(self) -- to test, but not yet complete
+    def Main(self):
+        Barcode.Main(self) # -- to test, but not yet complete
         
-    if __name__ == '__main__':
-        Main()
+
+System.Main()
     
