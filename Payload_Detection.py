a78399e876e20bf7a040d0a72e00d4e4ef081f8b
reginaleto <ginaleto@umich.edu>
2023-10-18 10:24:50 -0400

    Notes Added from 10/17

--- Payload_Detection.py
+++ Payload_Detection.py
@@ -1,6 +1,7 @@
 import picamera2
 import cv2
 
+
 def Detect_Red():   
     
     # return color string into self.Payload_Color
@@ -21,7 +22,8 @@ def Main(self):
         self.Payload_Color = Detect_Red()
         self.Payload_Color = Detect_Blue()
         self.Payload_Color = Detect_Green()
-
-
+        
+    
 if __name__ == '__main__':
     Main()
+    
