c19fd66a4b1b06a2f599c70adad23377be05a321
reginaleto <ginaleto@umich.edu>
2023-10-23 11:40:39 -0400

    Barcodes Generated
    Final Barcodes generated in Final_Barcodes folder within Barcodes
    
    Instruction Decode algorithm adjusted to account for extra char at end of number strings since barcode 1 and barcode 6 both include 6's

--- Barcode_Gen.py
+++ Barcode_Gen.py
@@ -4,9 +4,9 @@ from barcode.writer import ImageWriter
 
 def Main():
     # Barcode with ID Number associated with turning left
-    barcode_id = '333333333333' # ID number must be 12 digits
+    barcode_id = '666666666666666666' # ID number must be 12 digits
     barcode = EAN13(barcode_id, writer=ImageWriter()) # generates barcode with specific ID number and converts to PNG
-    barcode.save('/home/ginaleto/ECE_4983-4984_Path_Following_Delivery_Robot/Barcodes/Instruction3_Barcode_Test') # no need to specify file ending bc of above line
+    barcode.save('/Users/ginaleto/ECE_4983-4984_Path_Following_Delivery_Robot/Barcodes/Final_Barcodes/Instruction6_Loading_Station') # no need to specify file ending bc of above line
 
 if __name__ == '__main__':
     Main()
