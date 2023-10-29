75760abbd57dcd438eed26bf63edf8db7e3ba134
reginaleto <ginaleto@umich.edu>
2023-10-29 16:18:16 -0400

    10/29

--- System.py
+++ System.py
@@ -1,19 +1,21 @@
 import Instruction_Decode as Barcode
+import Payload_Detection as Color_Detect
 # import Payload_Manipulation as Forklift
 # import Motor_Control as Motors
 # import Streaming
 # import Sensor_Integration as Input
 
 
+class System(): 
+    def __init__(self):
+        self.Payload_Color = None
+        self.Previous_Color = None
+        self.ID = None
 
-def __init__(self):
-    self.Payload_Color = None
-    self.ID = None
-    self.Previous_Color = None
-    
-def Main(self):
-    Barcode.Main(self) # -- to test, but not yet complete
-    
-if __name__ == '__main__':
-    Main()
+    def Main(self):
+        Color_Detect.Main(self)
+       # Barcode.Main(self)
+
+system_test = System()
+system_test.Main()
     
